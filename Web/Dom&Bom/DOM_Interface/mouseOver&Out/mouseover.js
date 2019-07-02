cd5af67527168e0c86afb527249d09b271187d86
Christopher Wang <christopher.wang@zoom.us>
2019-07-02 09:09:06 +0800

    little thinking about tooltip according to bootstrape tooltip

--- Web/Dom&Bom/DOM_Interface/mouseOver&Out/mouseover.js
+++ Web/Dom&Bom/DOM_Interface/mouseOver&Out/mouseover.js
@@ -17,20 +17,54 @@ function offsetEle(ele){
         height: height 
     }
 }
-function tooltip(e){
-    let tooltipEle = document.createElement('div');
-    let offset = offsetEle(e); 
-    tooltipEle.style = `
+
+function Tooltip(selector){
+    this.element = document.querySelector(selector);
+    this.uid = uid++;
+    this.selector = selector;
+    this.state = 'hide';
+}
+
+Tooltip.uid = 1;
+Tooltip.template = `
+    <div class="tooltip" role="tooltip">
+        <div class="arrow"></div>
+        <div class="tooltip-inner"></div>
+    </div> 
+`
+Tooltip.prototype.show = function(){
+    let ttEle = document.createElement('div');
+    let offset = offsetEle(this.element); 
+    ttEle.outerHTML = Tooltip.template;
+    this.element.setAttribute('data-tooltip-id', this.uid);
+    ttEle.style = `
+        display: block;
         position: absolute;
         top: 0;
         left: 0;
         transform: translate(${offset.left}px, ${offset.top - offset.height}px);
         background: lime;
     `  
-    tooltipEle.innerText = "hello tooltip";
-    document.body.appendChild(tooltipEle);
-    console.log(e.tagName, offset);
+    //set tooltip text here
+    ttEle.querySelector('tooltip-inner').innerText = "Hello Tooltip";
+    this.state = 'show';
+    document.body.appendChild(ttEle);
+    return this;  
 }
+Tooltip.prototype.hide = function(){
+    this.state = 'hide';
+    let ttEle = document.querySelector(`[data-tooltip-id="${this.uid}"]`);
+    ttEle.style.display = 'none';
+    return this;
+}
+
+Tooltip.prototype.dispose = function(){
+    let ttEle = document.querySelector(`[data-tooltip-id="${this.uid}"]`);
+    this.element.removeAttribute('data-tooltip-id');
+    ttEle.remove();
+    return null;
+}
+
 function over(e){
     if(e.target){
         tooltip(e.target);
--- Web/Dom&Bom/DOM_Interface/mouseOver&Out/notes.md
+++ Web/Dom&Bom/DOM_Interface/mouseOver&Out/notes.md
@@ -1,3 +1,9 @@
+$('#example').tooltip(options)
+             .tooltip('show')
+             .tooltip('hide')
+             .tooltip('dispose')
+             .tooltip('toogle')
+
 1. 为某个元素添加一个tooltip 要提供一个怎样的api
 2. tooltip元素，应该被插入dom的什么位置
     * ele的子元素
@@ -8,3 +14,15 @@
 2. tooltip(eleA, content, options);
 
 ## 如何然tooltip在附主上水平居中，（不知晓tooltip的宽度的情况下)
+
+
+1. 把tooltip放在body中
+2. 如何为某一个元素添加tooltip ??
+3. 如何隐藏改元素的tooltip ??
+4. 某个元素重复添加tooltip的时候如何处理，避免在body中添加属于同一个元素的tooltip ??
+
+5. 当有多个元素都需要添加tooltip的时候
+    1. 如何标识某个tooltip属于某个element ？？
+    那么每个tooltip都要有一个id，这个id要放在element那里
+    有一个集中的地方，来为每个tooltip分配一个id
+    有一个集中的地方，根据tooltip的id来对tooltip进行处理
