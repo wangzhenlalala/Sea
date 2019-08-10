d392d7511410c91a05e1af5ba6c78b187380e75c
Christopher Wang <christopher.wang@zoom.us>
2019-08-10 14:20:49 +0800

    触发强制重排

--- Web/Performance/experiments/force_sync_layout/index.js
+++ Web/Performance/experiments/force_sync_layout/index.js
@@ -1,68 +1,16 @@
-function addElements(){
-    let _num = 20;
-    let number = _num;
-    let domFragment = document.createDocumentFragment();
-    while(number--){
-        let divEle = document.createElement('div');
-        divEle.classList.add('classA');
-        divEle.classList.add('classB');
-        divEle.style.position = 'relative';
-        divEle.style.left = (_num - number) * 10 + 'px';
-        divEle.style.background = `rgb(${number * 12}, ${(255-number*12)}, ${number*3})`;
-        domFragment.appendChild(divEle);
+
+let divEle = document.querySelector('.div1');
+let paraEles = document.querySelectorAll('.p_p');
+function onClickDocument(e){
+    for (var p = 0; p < paraEles.length; p++) { 
+        var para = paraEles[p]; 
+        /*
+        第一次读取的时候，divEle's style is invalidated
+        第二次读取的时候，就 Recalculate Style -> Layout，并且layout root是 #document
+        */
+        var width = divEle.offsetWidth; 
+        para.style.left = width + 'px'; 
     }
-    document.body.appendChild(domFragment);
 }
 
-
-addElements();
-
-
-document.addEventListener("click", function(){
-    // window.requestAnimationFrame(function(){
-    //     let divEles = document.querySelectorAll('.classA');
-    //     console.timeStamp('before get style');
-    //     divEles.forEach(function(ele){
-    //         // console.log( getPropFromStyle('width', ele) );
-            
-    //         console.log( getPropFromComputedStyle('width', ele) );
-    //     })
-    //     console.timeStamp('after get style');
-    // })
-
-    let divEles = document.querySelectorAll('.classA');
-    console.timeStamp('before get style');
-    divEles.forEach(function(ele){
-        // console.log( getPropFromStyle('width', ele) );
-        
-        console.log( getPropFromComputedStyle('width', ele) );
-        // console.log( ele.offsetWidth)
-        console.log( ele.offsetLeft)
-    })
-    console.timeStamp('after get style');
-});
-
-function getPropFromStyle(prop, ele){
-    return ele.style[prop];
-};
-
-function setPropToStyle(prop, value, ele){
-    return ele.style[prop] =  value;
-}
-function getPropFromComputedStyle(prop, ele){
-    return window.getComputedStyle(ele)[prop];
-}
-
-
-function getScrollbarWidth() {
-    var scrollDiv = document.createElement('div');
-    scrollDiv.className = 'modal-scrollbar-measure';
-    document.body.appendChild(scrollDiv);
-    var rect = scrollDiv.getBoundingClientRect();
-    var scrollbarWidth = rect.width - scrollDiv.clientWidth;
-    console.log(rect);
-    console.log(rect.width, scrollDiv.clientWidth);
-    document.body.removeChild(scrollDiv);
-   
-    return scrollbarWidth;
-}
+document.addEventListener('click', onClickDocument);
--- Web/Performance/performance.md
+++ Web/Performance/performance.md
@@ -39,6 +39,8 @@
 
 <https://developers.google.com/web/tools/chrome-devtools/evaluate-performance/>
 
+<https://googlechrome.github.io/devtools-samples/jank/>
+
 
 > The problem with this code is that, in each animation frame, 
 it changes the style for each square, and then queries the position of each square on the page. 
@@ -47,7 +49,21 @@ so it has to re-layout the square in order to compute its position.
 	
 ![](./images/reflow.png)
 ![first layout invalidaton](./images/first-layout-invalidation.png)
-	
-	
-	
-	
+
+```js
+    for (var p = 0; p < paragraphs.length; p++) { 
+        var para = paragraphs[p]; 
+        var width = div.offsetWidth; 
+        para.style.left = width + 'px'; 
+    }
+    read, write, read(relayout), write, read(relayout),write
+    ///////////////////////////////////////////////////////
+    var width = div.offsetWidth; 
+    for (var p = 0; p < paragraphs.length; p++) { 
+        var para = paragraphs[p]; 
+        para.style.left = width + 'px'; 
+    }
+    read, write, write ,write
+```	
+
+> In this example I’m looping through a bunch of paragraphs and setting the width of each to match some target div. When you map this to the work that the browser has to do you’ll see that offsetWidth gets the width of the div, which requires a layout (read) and this is immediately followed by setting the first paragraph’s width (write). The write invalidates the read’s layout calculations because part of the render tree has changed. So when the browser hits the second paragraph it has to do layout again, and this is once again invalidated by mutating styles.
