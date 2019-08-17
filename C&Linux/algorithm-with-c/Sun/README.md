1741ef1be8f8796cbc24a9d20dd1f41db11e238d
wangzhen <thewhiteelephant@163.com>
2019-08-17 20:58:35 +0800

    list 没有删除

---
+++ C&Linux/algorithm-with-c/Sun/README.md
@@ -0,0 +1,5 @@
+## first time to write code in c
+1. 如何组织一个C语言项目的目录结构？
+2. 如何使用 make 来构建项目
+    * make 生成的.o a.out文件，要放到那里去呢？？
+    * 如何将最后生成的文件移动到/bin下面
---
+++ C&Linux/algorithm-with-c/Sun/src/list.c
@@ -0,0 +1,96 @@
+/*
+ 这里实现的list 在顶部添加了一个 sentinel node 
+ 为了在删除 list中的第一个元素的时候，不影响外部的使用
+*/
+
+
+#include "include/list.h"  //相对的路径,尽量不要写 ../** or ./**
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    ElementType Element;
+    Position Next;
+};
+
+int IsEmpty( List L )
+{
+    return L -> Next == NULL;
+}
+/*
+    return true if P is the last position in list L
+    parameter L is unused in this implementation
+    ?? what if P is not in L
+*/
+int IsLast( Position P, List L )
+{
+    return P->Next == NULL;
+}
+
+// Return Position of X in L;
+// NULL if not found
+Position Find( ElementType X, List L )
+{
+    Position First;
+    First = L->Next;
+    while(!First && First->Element != X)
+        First = First->Next;
+    return First;
+}
+
+/**
+ delete first occurrence of X from list L
+ assume use of a header node
+*/
+// _M 说明是我自己的实现，不是书中的实现
+void Delete_M( Position P, List L )
+{
+    Position Cur = L;
+    Position Pre = L->Next;
+    while(!Cur && Cur->Element != P->Element)
+        Pre = Cur;
+        Cur = Cur->Next;
+    if(!Cur) return; //do not in list
+    Pre->Next = Cur->Next;
+    free(Cur); // 因为我们的每个Node都是用 malloc 动态分配的
+    //we should do something to clean the deleted element eg. Cur.destory();
+}
+
+void Delete( ElementType X, List L) 
+{
+    Position P;
+    Position TmpCell;
+    P = FindPrevious( X, L );
+    // 如果是最后一个，说明 X 在 L 中不存在
+    if( !IsLast( P, L ) )
+    {
+        TmpCell = P->Next;
+        P->Next = TmpCell->Next;
+        free(TmpCell);
+    }
+}
+
+Position FindPrevious( ElementType X, List L )
+{
+    Position P;
+    P = L;
+    if(!P->Next && P->Next->Element != X )
+        P = P->Next;
+    return P; //如果 X在L中不存在， 就返回最后一个元素
+}
+
+// 这里假定 P 是一个合法的位置；
+void Insert( ElementType X, List L, Position P )
+{
+    Position TmpCell;
+    TmpCell = malloc( sizeof( struct Node ) );
+    if( !TmpCell )
+        FatalError( "Out of space!!!" );
+    TmpCell->Next = P->Next;
+    TmpCell->Element = X;
+    P->Next = TmpCell;
+}
+
+
+
+
