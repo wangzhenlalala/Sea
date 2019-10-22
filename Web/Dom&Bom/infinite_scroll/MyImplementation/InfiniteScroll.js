338208de15aae49fdf582ef452a05ba3862d2a0c
Christopher Wang <christopher.wang@zoom.us>
2019-10-22 09:39:49 +0800

    明白了calculateAnchoredItem的过程

--- Web/Dom&Bom/infinite_scroll/MyImplementation/InfiniteScroll.js
+++ Web/Dom&Bom/infinite_scroll/MyImplementation/InfiniteScroll.js
@@ -1,3 +1,7 @@
+/*
+    data: 说明数据已经异步加载完成了
+    node: 该数据对应的dom节点，如果data == null, 则为tombstone，data != null, 说明是对应的节点
+*/
 let Item = {
     node: null, // dom or tombstone dom
     // height: 0, // 已知的
@@ -43,38 +47,58 @@ function InfiniteScroll(store, scroller, config) {
 InfiniteScroll.prototype = {
     onScroll(e) {
         e.preventDefault();
-        let scrollTop = e.target.scrollTop;
-        let delta = scrollTop - this._scrollTop;
-        this._anchorItem = this.calculateAnchoredItem(this._anchorItem, delta);
+        let delta = this._scroller.scrollTop - this._scrollTop;
+        this._scrollTop = e.target.scrollTop;
+        if(this._scrollTop == 0) {
+            this._anchorItem = { index: 0, offset: 0 };
+        }else {
+            this._anchorItem = this.calculateAnchoredItem(this._anchorItem, delta);
+        }
+        
         let endItem = this.calculateAnchoredItem(this._anchorItem, this._scroller.offsetHeight); // 每次调用了offsetset
+
+        // 这里只计算 startIndex 和 endIndex 不做边界处理
         if(delta > 0) {
             // 手指向上滑动, 看到下面的内容
             // 无限滚动是没有下限的
             // this.fill( Math.max(this._anchorItem.index - this.BACK_NUMBER, 0), Math.min(endItem.index + this.FRONT_NUMBER, this._items.length));
-            this.fill( Math.max(this._anchorItem.index - this.BACK_NUMBER, 0), endItem.index + this.FRONT_NUMBER, this._items.length);
+            this.fill( this._anchorItem.index - this.BACK_NUMBER, endItem.index + this.FRONT_NUMBER);
 
         } else {
             // 手指向下滑动， 看到上面的内容
-            this.fill( Math.max(this._anchorItem.index - this.FRONT_NUMBER, 0), endItem.index + this.BACK_NUMBER, this._items.length);
+            this.fill( this._anchorItem.index - this.FRONT_NUMBER, endItem.index + this.BACK_NUMBER);
         }
     },
     // 计算距离item offset长度的item,offset > 0 : 向下； offset < 0 : 向上 
     calculateAnchoredItem(anchorItem, offset) {
+        // 要考虑tombstone
         if(offset == 0) return anchorItem;
         let distance = 0;
         let index = anchorItem.index;
         let item = {};
         if(offset > 0) {
-            // 向下底对齐
-            distance = this._itemDomHeight - anchorItem.offset;
-            while(true) {
-                if(distance >= offset) {
-                    item.index = index;
-                    item.offset = this._itemDomHeight - (distance - offset);
+            // 向下底对齐 
+            // 有可能会遇到底部啊
+            // 当前的元素有可能都是，tombstone
+            // distance = this._itemDomHeight - anchorItem.offset; 
+            
+            if(this._items[index].data == null) {
+                // tombstone节点, 由于数据是成批到来的，那么再改节点之后的节点都为tomebsone节点
+                distance = this._tombstoneHeight - anchorItem.offset; 
+                index = index + Math.floor((delta - distance) / this._tombstoneHeight);
+                offset = 
+
+            } else {
+                distance = this._itemDomHeight - anchorItem.offset;
+                while(true) {
+                    if(distance >= offset) {
+                        item.index = index;
+                        item.offset = this._itemDomHeight - (distance - offset);
+                    }
+                    distance += this._itemDomHeight; // 这么算根本就没有考虑tombstone
+                    index++;
                 }
-                distance += this._itemDomHeight; // 这么算根本就没有考虑tombstone
-                index++;
-            }
+            }            
         } else {
             // 向上顶对齐
             distance = anchorItem.offset;
