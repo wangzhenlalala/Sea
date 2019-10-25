c1bbc4770a39039945c8f15d176c27d4b4a15fe1
Christopher Wang <christopher.wang@zoom.us>
2019-10-25 13:14:13 +0800

    noon

--- Web/Dom&Bom/infinite_scroll/MyImplementation/InfiniteScroll.js
+++ Web/Dom&Bom/infinite_scroll/MyImplementation/InfiniteScroll.js
@@ -1,21 +1,16 @@
 function InfiniteScroll(store, scroller, config) {
-    this._store = store;
-    this._itemDomHeight = config.itemHeight; // 每个item的高度
-    this._tombstoneHeight = config.tombstoneHeight;
-    this._scroller = scroller;
-    this._items = [];
-    this._scrollTop = 0;
+    this.item_height = 0; // 每个item的高度
+    this.vierport_height = 0;
+    this.scroller = scroller;
+    this.item_list = [];
+    this.scrollTop = 0;
+    this.anchorIndex = 0;
     
-   
-    
-    this._startIndex = 0;
-    this._endIndex = 0; // 生成dom，的item的范围 _endIndex也会被渲染出来
     this.FRONT_NUMBER = 3; // 滚动方向，前方的数量
     this.BACK_NUMBER = 3;
     this._sentinelTranslateY = 0;
-    this._distanceAfterEndIndex = 200;
     // add scroll listener to scroller
-    scroller.addEventListener('scroll', this.onScroll.bind(this));
+    scroller.addEventListener('scroll', this.refresh.bind(this));
 
     // insert sentinel
     this._sentinel = document.createElement('div');
@@ -26,18 +21,18 @@ function InfiniteScroll(store, scroller, config) {
         height: 1px;
         opacity: 0;
     `;
-    this._scroller.appendChild(this._sentinel);
-
-    // set it go 
-    this.onScroll();
+    this.scroller.appendChild(this._sentinel);
 }
 
 InfiniteScroll.prototype = {
     onScroll() {
         
     },
-   
-    
+    // 调用者手动制定
+    // 拖动触发 resize 事件 
+    onResize(viewport_height, vierport_width) {
+
+    },
     attachContent() {
         // 每个item记录了每个dom节点的所有信息， 根据游标范围来渲染数据
         // 更新 [_startIndex, _endIndex]范围内的item
@@ -73,11 +68,18 @@ InfiniteScroll.prototype = {
          * viewport height
          * item height 
          * item list 用来计算 应该渲染哪些元素 start index, end index
+         *      长度 变化
+         *      顺序 变化
+         *      内容 变化
          */
     },
     refresh(viewport_height, item_height, item_list) {
+        this.viewport_height = viewport_height;
+        this.item_height = item_height;
+        this.item_list = item_list;
         // 1. 更新实例的数据
         // 2. 计算应该渲染哪些元素
+        let preAncorItem = 
         // 3. 更新范围内元素的dom
         // 4. 更新当前的状态
     }
