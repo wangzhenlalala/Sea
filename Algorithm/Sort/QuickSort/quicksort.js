04eab733ebaf722304fde4531fb0d6c95f6f92f2
Christopher Wang <christopher.wang@zoom.us>
2019-10-29 08:49:26 +0800

    调整qucicksort,要上班了

--- Algorithm/Sort/QuickSort/quicksort.js
+++ Algorithm/Sort/QuickSort/quicksort.js
@@ -1,33 +1,33 @@
 class QuickSort {
-	static Sort(list) {
-		QuickSort.xx(list, 0, list.length - 1);
+	static Sort(list) { // 大写的sort
+		QuickSort.sort(list, 0, list.length - 1);
 	}
-	static xx(list, startIndex, endIndex) {
+	static sort(list, startIndex, endIndex) { // 消息的sort
 		if(startIndex >= endIndex) return;
-		let pivotIndex = QuickSort.sort_(list, startIndex, endIndex);
-		QuickSort.xx(list, startIndex, pivotIndex - 1);
-		QuickSort.xx(list, pivotIndex + 1, endIndex);
+		let pivotIndex = QuickSort.partition(list, startIndex, endIndex);
+		QuickSort.sort(list, startIndex, pivotIndex - 1);
+		QuickSort.sort(list, pivotIndex + 1, endIndex);
 	}
 	// Array[a] => int => int => int
-	static sort_(list, startIndex, endIndex) {
+	static partition(list, startIndex, endIndex) {
 		/* 将【start - end]范围内的元素，分成三部分 
 		union ( 
 			[x <- list | x <= pivot], 
 			[pivot], 
-			[x <- list | x > pivot] 
+			[x <- list | x >= pivot]
 		) where pivot = list[startIndex];
 		*/ 
 		if(startIndex >= endIndex) return startIndex; // 要返回一个pivot 的index， 但是这里我们直接返回了，是undedined
 		let i = startIndex; // x <= i ==> list[x] <= pivot
 		let j = endIndex + 1; // x >= j && exist(list[x]) ==> list[x] > pivot;
-		while( i < j ) {
-			while( QuickSort.lessEq(list[++i], list[startIndex]) ) {
-				if(i >= j) break;
+		while( true ) {
+			while( QuickSort.lessEq(list[++i], list[startIndex]) ) { // 小于等于
+				if(i == endIndex) break;
 			}
-			while(!QuickSort.lessEq(list[--j], list[startIndex])) {
-				if(j < i) break;
+			while( QuickSort.lessEq(list[startIndex], list[--j]) ) { // 大于等于
+				if(j == startIndex) break;
 			}
-			if(i < j) {
+			if(i < j) { // important
 				QuickSort.swap(list, i, j);
 			}
 			else {
@@ -39,24 +39,28 @@ class QuickSort {
 		return j;
 	}
 	static lessEq(itemA, itemB) {
-		return itemA - itemB <= 0
+		return itemA <= itemB;
 	}
+
 	static less(itemA, itemB) {
-		return itemA - itemB;
+		return itemA < itemB;
 	}
+
 	static swap(list, i, j) {
 		let temp = list[j];
 		list[j] = list[i];
 		list[i] = temp;
 	}
+
 	static isSorted(list){
-		for(let i=0; i<list.length-1; i++) {
+		for(let i = 0; i < list.length - 1; i++) {
 			if(!QuickSort.lessEq(list[i], list[i+1])) return false;
 		}
 		return true;
 	}
+
 	static main() {
-		// for tes
+		// for test
 		let list = [];
 		for(let i=0; i<20; i++) {
 			list.push(chance.integer({min: 1, max: 9999}));
